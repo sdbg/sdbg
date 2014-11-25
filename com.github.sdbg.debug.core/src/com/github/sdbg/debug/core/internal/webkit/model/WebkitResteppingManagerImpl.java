28f5c2350f3d82f314f1d24748c45719b9bbb096
ivmarkov <ivan.markov@gmail.com>
2014-11-25 14:16:52 +0200

    Make the smart restepping optional, mark it as experimental and turn it off by default

--- com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/webkit/model/WebkitResteppingManagerImpl.java
+++ com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/webkit/model/WebkitResteppingManagerImpl.java
@@ -1,5 +1,6 @@
 package com.github.sdbg.debug.core.internal.webkit.model;
 
+import com.github.sdbg.debug.core.SDBGDebugCorePlugin;
 import com.github.sdbg.debug.core.internal.webkit.protocol.WebkitCallFrame;
 import com.github.sdbg.debug.core.internal.webkit.protocol.WebkitDebugger.PausedReasonType;
 import com.github.sdbg.debug.core.internal.webkit.protocol.WebkitLocation;
@@ -49,10 +50,11 @@ public class WebkitResteppingManagerImpl implements WebkitResteppingManager {
             location.getLineNumber(),
             location.getColumnNumber());
 
-        if (currentLocation == null && stepLocation != null
+        if (SDBGDebugCorePlugin.getPlugin().getUseSmartStepInOut() && currentLocation == null
+            && stepLocation != null
             && (stepCommand.equals("Debugger.stepInto") || stepCommand.equals("Debugger.stepOut"))) {
           // Filter out frames that are NOT sourcemapped but are otherwise inside sourcemapped files
-          restepCommand = "Debugger.stepOut";
+          restepCommand = stepCommand; //"Debugger.stepOut";
           restep = true;
           return;
         }
@@ -63,8 +65,9 @@ public class WebkitResteppingManagerImpl implements WebkitResteppingManager {
 
     // Restepping is needed if we are still on the same line in the same Java source file
     restepCommand = null;
-    restep = exception == null && reason == PausedReasonType.other && currentLocation != null
-        && stepLocation != null && currentLocation.getPath().equals(stepLocation.getPath())
+    restep = SDBGDebugCorePlugin.getPlugin().getUseSmartStepOver() && exception == null
+        && reason == PausedReasonType.other && currentLocation != null && stepLocation != null
+        && currentLocation.getPath().equals(stepLocation.getPath())
         && currentLocation.getLine() == stepLocation.getLine();
 
     if (!restep) {

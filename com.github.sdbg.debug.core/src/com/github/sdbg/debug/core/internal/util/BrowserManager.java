918f0a22dac1fba221bd93ea337d4b0f8f6bcced
ivmarkov <ivan.markov@gmail.com>
2015-01-31 18:45:52 +0200

    #113

--- com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/util/BrowserManager.java
+++ com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/util/BrowserManager.java
@@ -36,6 +36,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Semaphore;
@@ -831,7 +832,7 @@ public class BrowserManager {
                   launch,
                   launchConfig,
                   DebugPlugin.newProcess(launch, browserProcess, browserExecutable.getName()
-                      + " (Run only, debugging DISABLED)"),
+                      + " - Run only, debugging DISABLED (" + new Date() + ")"),
                   processDescription.toString());
             }
           }

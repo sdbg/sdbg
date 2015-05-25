8cba489eb40a71946435a341af2fa5d7a9742bca
ivmarkov <ivan.markov@gmail.com>
2015-05-25 22:56:59 +0300

    #100

--- com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/webkit/model/WebkitResteppingManagerImpl.java
+++ com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/webkit/model/WebkitResteppingManagerImpl.java
@@ -16,7 +16,6 @@ public class WebkitResteppingManagerImpl implements WebkitResteppingManager {
 
   private String stepCommand;
 
-  private SourceMapManager.SourceLocation currentLocation;
   private SourceMapManager.SourceLocation stepLocation;
 
   private boolean restep;
@@ -39,6 +38,7 @@ public class WebkitResteppingManagerImpl implements WebkitResteppingManager {
   @Override
   public void onDebuggerPaused(List<WebkitCallFrame> frames, PausedReasonType reason,
       WebkitRemoteObject exception) {
+    SourceMapManager.SourceLocation currentLocation = null;
     WebkitCallFrame frame = frames.isEmpty() ? null : frames.get(0);
     if (frame != null) {
       IStorage storage = target.getScriptStorageFor(frame);
@@ -49,35 +49,32 @@ public class WebkitResteppingManagerImpl implements WebkitResteppingManager {
             storage,
             location.getLineNumber(),
             location.getColumnNumber());
-
-        if (SDBGDebugCorePlugin.getPlugin().getUseSmartStepInOut() && currentLocation == null
-            && stepLocation != null
-            && (stepCommand.equals("Debugger.stepInto") || stepCommand.equals("Debugger.stepOut"))) {
-          // Filter out frames that are NOT sourcemapped but are otherwise inside sourcemapped files
-          restepCommand = stepCommand; //"Debugger.stepOut";
-          restep = true;
-          return;
-        }
       }
-    } else {
-      currentLocation = null;
     }
 
-    // Restepping is needed if we are still on the same line in the same Java source file
-    restepCommand = null;
-    restep = SDBGDebugCorePlugin.getPlugin().getUseSmartStepOver() && exception == null
+    if (SDBGDebugCorePlugin.getPlugin().getUseSmartStepInOut() && currentLocation == null
+        && stepLocation != null) {
+      // Filter out frames that are NOT sourcemapped but are otherwise inside sourcemapped files
+      restepCommand = stepCommand.equals("Debugger.stepOver") ? "Debugger.stepInto" : stepCommand;
+      restep = true;
+      return;
+    } else if (SDBGDebugCorePlugin.getPlugin().getUseSmartStepOver() && exception == null
         && reason == PausedReasonType.other && currentLocation != null && stepLocation != null
         && currentLocation.getPath().equals(stepLocation.getPath())
-        && currentLocation.getLine() == stepLocation.getLine();
-
-    if (!restep) {
-      stepLocation = null;
+        && currentLocation.getLine() == stepLocation.getLine()) {
+      // Restepping is needed if we are still on the same line in the same Java source file
+      // TODO: The above comparison is way too simplistic, the whole set of stackframes should be compared
+      restepCommand = null;
+      restep = true;
+      return;
     }
+
+    stepLocation = currentLocation;
+    restep = false;
   }
 
   @Override
   public void onStepping(String stepCommand) {
     this.stepCommand = stepCommand;
-    stepLocation = currentLocation;
   }
 }

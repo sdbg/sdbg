2e4a88a614b071dae451ddd2e1c5810db349f170
keinhaar <10001689+keinhaar@users.noreply.github.com>
2023-03-06 14:42:13 +0100

    More Firefox Variables
    - Added "this" to the shown variables
    - Allow to inspect Object variables recursive
    - Removed browser.location from launch config

--- com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/browser/firefox/FirefoxDebugValue.java
+++ com.github.sdbg.debug.core/src/com/github/sdbg/debug/core/internal/browser/firefox/FirefoxDebugValue.java
@@ -1,12 +1,17 @@
 package com.github.sdbg.debug.core.internal.browser.firefox;
 
+import com.github.sdbg.debug.core.SDBGDebugCorePlugin;
 import com.github.sdbg.debug.core.internal.webkit.model.WebkitDebugElement;
 import com.github.sdbg.debug.core.model.ISDBGValue;
+
+import java.util.Map;
 
 import org.eclipse.debug.core.DebugException;
 import org.eclipse.debug.core.model.IValue;
 import org.eclipse.debug.core.model.IVariable;
 import org.eclipse.debug.core.model.IWatchExpressionListener;
+
+import de.exware.remotefox.ObjectActor;
 
 public class FirefoxDebugValue extends WebkitDebugElement implements IValue, ISDBGValue
 {
@@ -27,19 +32,44 @@ public class FirefoxDebugValue extends WebkitDebugElement implements IValue, ISD
     @Override
     public String getValueString() throws DebugException
     {
+        if(value instanceof ObjectActor)
+        {
+            ObjectActor actor = (ObjectActor)value;
+            String id = actor.getActorId();
+            return actor.getClassName() + " " + id.substring(id.lastIndexOf("/obj") + 4);
+        }
         return value == null ? "null" : value.toString();
     }
 
     @Override
     public IVariable[] getVariables() throws DebugException
     {
-        return null;
+        IVariable[] vars = null;
+        if(value instanceof ObjectActor)
+        {
+            Map<String, Object> map;
+            try
+            {
+                map = ((ObjectActor)value).getProperties();
+            }
+            catch (Exception e)
+            {
+                throw new DebugException(SDBGDebugCorePlugin.createErrorStatus("Unable to load variables"));
+            }
+            vars = new IVariable[map.size()];
+            int i = 0;
+            for (String name : map.keySet())
+            {
+                vars[i++] = new FirefoxDebugVariable((FirefoxDebugTarget) getDebugTarget(), name, map.get(name));
+            }
+        }
+        return vars;
     }
 
     @Override
     public boolean hasVariables() throws DebugException
     {
-        return false;
+        return value instanceof ObjectActor;
     }
 
     @Override
